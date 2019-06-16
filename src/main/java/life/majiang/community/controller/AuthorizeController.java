b2d8a6d3d55ab31348b49a79c6e6459db3598554
wangabeng <wangabeng@163.com>
2019-06-16 13:23:19 +0800

    '完善'

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,6 +2,7 @@ package life.majiang.community.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,12 @@ import life.majiang.community.provider.GithubProvider;
 public class AuthorizeController {
 	@Autowired
 	private GithubProvider githubProvider; // 自动注入类
+	
+	@Value("${github.client.id}")
+	private String clientId;
+	@Value("${github.client.secret}")
+	private String clientSecret;
+	
 	@GetMapping("/callback")
 	public String callback (@RequestParam(name = "code") String code) {
 		// 跳转携带code的时候返回
@@ -22,16 +29,18 @@ public class AuthorizeController {
 		// 创建GithubProvider类 发送post请求
 		AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 		// String jsonStr = "{'client_id': '840e14b3990e1a66b140','client_secret': 'f0bbc0eecef590d7abbd24515320573630701364'}";
-		accessTokenDTO.setClient_id("840e14b3990e1a66b140");
-		accessTokenDTO.setClient_secret("f0bbc0eecef590d7abbd24515320573630701364");
+		accessTokenDTO.setClient_id(clientId);
+		accessTokenDTO.setClient_secret(clientSecret);
 		accessTokenDTO.setCode(code);
 		
 		// 获取到access token字符串 需要截取
 		String acessToken = githubProvider.getAccessToken(accessTokenDTO);
+		
+		// 获取用户信息
 		GithubUser user = githubProvider.getUser(acessToken);
-		System.out.print("beigin--------");
-		System.out.print(user.getName()); //  成功
-		System.out.print("end--------");
+		// System.out.print("beigin--------");
+		// System.out.print(user.getName()); //  成功
+		// System.out.print("end--------");
 		
 		return "index";
 	}
